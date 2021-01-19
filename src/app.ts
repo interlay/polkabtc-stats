72f2878a5dd57644eef123c3eee48135d3bbafe1
Theodore Bugnet <theodore@interlay.io>
2021-01-19 19:51:34 +0000

    feat(config): improved generation configuration, added CORS handling

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,13 @@ app.use(
 );
 app.use(bodyParser.json());
 
+app.use((_, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
   return res.send(
     swaggerUi.generateHTML(await import("../build/swagger.json"))
--- src/issue/issueDataService.ts
+++ src/issue/issueDataService.ts
@@ -6,7 +6,7 @@ export async function getSuccessfulIssues(): Promise<string> {
     try {
         const res = await pool.query("select count(*) from v_parachain_executed_issues");
         console.log(res.rows[0]);
-        return res.rows[0];
+        return res.rows[0].count;
     }
     catch (e) {
         console.error(e);
