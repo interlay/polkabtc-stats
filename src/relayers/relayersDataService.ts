8aeed4a7324e57c93af26a3590e344b4d7f1230c
Theodore Bugnet <theodore@interlay.io>
2021-02-22 17:54:54 +0000

    fix(relayers): significantly fixed relayer queries, to a) account for repeated registering and deregistering, and b) account for the bonding period in active counts.

--- src/relayers/relayersDataService.ts
+++ src/relayers/relayersDataService.ts
@@ -1,6 +1,7 @@
 import { Relayer, RelayerCountTimeData} from "./relayersModel";
 import { runPerDayQuery } from "../common/util";
 import pool from "../common/pool";
+import {planckToDOT} from "@interlay/polkabtc";
 
 export async function getRecentDailyRelayers(
     daysBack: number
@@ -13,13 +14,21 @@ export async function getRecentDailyRelayers(
                 `
                 SELECT
                     ${i} AS idx,
-                    reg - dereg AS value
+                    GREATEST(reg - dereg, 0) AS value
                 FROM
                     (
                         SELECT
                             COUNT(relayer_id) AS reg
                         FROM v_parachain_stakedrelayer_register
-                        WHERE block_ts < '${ts}'
+                        LEFT OUTER JOIN
+                            (SELECT block_number
+                            FROM parachain_events
+                            ORDER BY block_number DESC
+                            LIMIT 1) latestblock
+                        ON TRUE
+                        WHERE
+                            block_ts < '${ts}'
+                            AND maturity::Integer < latestblock.block_number
                     ) as r,
                     (
                         SELECT
@@ -39,34 +48,37 @@ export async function getRecentDailyRelayers(
 export async function getAllRelayers(): Promise<Relayer[]> {
     try {
         const res = await pool.query(`
-        SELECT
-            relayer_id,
-            stake,
-            COALESCE(deregistered, FALSE) AS deregistered,
-            COALESCE(slashed, FALSE) AS slashed,
-            maturity::Integer < latestblock.block_number AS bonded
-        FROM
-            v_parachain_stakedrelayer_register
-            LEFT OUTER JOIN
-                (SELECT
-                    relayer_id, TRUE as deregistered
-                FROM v_parachain_stakedrelayer_deregister)
-            AS dereg USING (relayer_id)
-            LEFT OUTER JOIN
-                (SELECT
-                    relayer_id, TRUE as slashed
-                FROM v_parachain_stakedrelayer_slash)
-            AS slash USING (relayer_id)
-            LEFT OUTER JOIN
-                (SELECT block_number
-                FROM parachain_events
-                ORDER BY block_number DESC
-                LIMIT 1)
-            AS latestblock ON TRUE
-        `);
+            SELECT DISTINCT ON (reg.relayer_id)
+                reg.relayer_id,
+                reg.stake,
+                COALESCE(deregistered, FALSE) deregistered,
+                COALESCE(slashed, FALSE) slashed,
+                maturity::Integer < latestblock.block_number bonded
+            FROM
+                v_parachain_stakedrelayer_register reg
+                LEFT OUTER JOIN
+                    (SELECT DISTINCT ON (relayer_id)
+                        relayer_id, block_number, TRUE deregistered
+                    FROM v_parachain_stakedrelayer_deregister
+                    ORDER BY relayer_id, block_number DESC) dereg
+                ON reg.relayer_id = dereg.relayer_id AND reg.block_number < dereg.block_number
+                LEFT OUTER JOIN
+                    (SELECT DISTINCT ON (relayer_id)
+                        relayer_id, TRUE as slashed
+                    FROM v_parachain_stakedrelayer_slash
+                    ORDER BY relayer_id) slash
+                ON reg.relayer_id = slash.relayer_id
+                LEFT OUTER JOIN
+                    (SELECT block_number
+                    FROM parachain_events
+                    ORDER BY block_number DESC
+                    LIMIT 1) latestblock
+                ON TRUE
+                ORDER BY reg.relayer_id, reg.block_number DESC
+            `);
         return res.rows.filter((row) => !row.deregistered).map((row) => ({
             id: row.relayer_id,
-            stake: row.stake,
+            stake: planckToDOT(row.stake),
             bonded: row.bonded,
             slashed: row.slashed,
         }));
