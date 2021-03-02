a4da5b9c14bdcc849cfa04b1eee72fce248a2a54
ns212 <nikolai@interlay.io>
2021-03-02 17:32:47 +0000

    Read from db replica host if env var is set

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -4,3 +4,4 @@ export const ENDPOINT_URL = process.env.ENDPOINT_URL || "ws://localhost:9944";
 export const BTC_NETWORK = process.env.BTC_NETWORK || "http://localhost:3002"; // for polkabtc-js, regtest must be a URL
 export const ENABLE_PG_SSL =
     process.env.PGSSLMODE && process.env.PGSSLMODE === "require";
+export const PGREPLICAHOST = process.env.PGREPLICAHOST || process.env.PGHOST
