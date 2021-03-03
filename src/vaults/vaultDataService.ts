4f0331e4d70f1e1776647ade2a028a7ca381331c
ns212 <nikolai@interlay.io>
2021-03-03 09:47:21 +0000

    Fix getRecentDailyVaults query

--- src/vaults/vaultDataService.ts
+++ src/vaults/vaultDataService.ts
@@ -19,7 +19,7 @@ export async function getRecentDailyVaults(
 ): Promise<VaultCountTimeData[]> {
     try {
         return (await pool.query(`
-        SELECT extract(epoch from d.date) * 1000 as date, count(v.vault_id)
+        SELECT extract(epoch from d.date) * 1000 as date, count(v.vault_id) as value
         FROM (SELECT (current_date - offs) AS date FROM generate_series(0, ${daysBack}, 1) AS offs) d
         LEFT OUTER JOIN v_parachain_vault_registration v
         ON d.date = v.block_ts::date
